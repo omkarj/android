044de17583484eb20b651a21790cd3bfd6ba4062
Omkar Jadhav <jadhav_o@India.XoriantCorp.com>
2017-09-20 15:39:06 +0530

    pedometer

---
+++ app/src/main/java/com/example/jadhav_o/pedometer/StepListener.java
@@ -0,0 +1,12 @@
+package com.example.jadhav_o.pedometer;
+
+/**
+ * Created by jadhav_o on 20-09-2017.
+ */
+//Source : http://www.gadgetsaint.com/android/create-pedometer-step-counter-android/#.WcHYRoyCzIU
+
+public interface StepListener {
+        public void step(long timeNs);
+
+    }
+
